4091d5a0943eddbf201d8de957ac2c84ea94132a
Lingyu-ILY <lingyu2022352@gmail.com>
2024-12-18 15:03:37 +0800

    style edit

--- live-embed.js
+++ live-embed.js
@@ -10,7 +10,7 @@ class LiveEmbed {
     let msgEmbed = new EmbedBuilder();
     
     msgEmbed.setColor(isLive ? "#9146ff" : "#808080");
-    //msgEmbed.setURL(`https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()}`);
+    msgEmbed.setURL(`https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()}`);
     
     // Thumbnail
     let thumbUrl = streamData.profile_image_url;
@@ -23,13 +23,13 @@ class LiveEmbed {
     
     if (isLive) {
       // Title
-      //msgEmbed.setTitle(`:red_circle: **${streamData.user_name} 正在 Twitch 上直播!**`);
-      msgEmbed.setDescription(streamData.title);
+      msgEmbed.setTitle(streamData.title);
+      //msgEmbed.setDescription(`:red_circle: 直播進行中 [前往觀看](https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()})`);
       //msgEmbed.addFields({ name: "標題", value: streamData.title, inline: false });
       msgEmbed.setAuthor({ name: `${streamData.user_name}正在 Twitch 上直播!`, iconURL: `${streamData.profile_image_url}`, url: `https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()}`});
     } else {
-      //msgEmbed.setTitle(`:white_circle: ${streamData.user_name} 曾在 Twitch 上直播.`);
-      msgEmbed.setDescription(streamData.title);
+      msgEmbed.setTitle(streamData.title);
+      //msgEmbed.setDescription(`:white_circle: 本次直播已結束 [前往頻道](https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()})`);
       //msgEmbed.addFields({ name: "標題", value: streamData.title, inline: true });
       msgEmbed.setAuthor({ name: `${streamData.user_name}曾在 Twitch 上直播.`, iconURL: `${streamData.profile_image_url}`, url: `https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()}`});
     }
