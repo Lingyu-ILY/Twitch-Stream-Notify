fee9cf1eeec1b624da20fdb7dee53807a750c67b
Lingyu-ILY <lingyu2022352@gmail.com>
2024-12-18 11:52:45 +0800

    edit style and test offline background thumb

--- live-embed.js
+++ live-embed.js
@@ -10,8 +10,7 @@ class LiveEmbed {
     let msgEmbed = new EmbedBuilder();
     
     msgEmbed.setColor(isLive ? "#9146ff" : "#808080");
-    msgEmbed.setURL(`https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()}`);
-    msgEmbed.setAuthor({ name: `${streamData.user_name}`, iconURL: `${streamData.profile_image_url}`});
+    //msgEmbed.setURL(`https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()}`);
     
     // Thumbnail
     let thumbUrl = streamData.profile_image_url;
@@ -24,12 +23,14 @@ class LiveEmbed {
     
     if (isLive) {
       // Title
-      msgEmbed.setTitle(`:red_circle: **${streamData.user_name} 正在 Twitch 上直播!**`);
-      msgEmbed.addFields({ name: "標題", value: streamData.title, inline: false });
+      //msgEmbed.setTitle(`:red_circle: **${streamData.user_name} 正在 Twitch 上直播!**`);
+      //msgEmbed.addFields({ name: "標題", value: streamData.title, inline: false });
+      msgEmbed.setAuthor({ name: `${streamData.user_name}正在 Twitch 上直播!`, iconURL: `${streamData.profile_image_url}`, url: 'https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()}'});
     } else {
-      msgEmbed.setTitle(`:white_circle: ${streamData.user_name} 曾在 Twitch 上直播.`);
-      msgEmbed.setDescription('本次直播已結束.');
-      msgEmbed.addFields({ name: "標題", value: streamData.title, inline: true });
+      //msgEmbed.setTitle(`:white_circle: ${streamData.user_name} 曾在 Twitch 上直播.`);
+      //msgEmbed.setDescription('本次直播已結束.');
+      //msgEmbed.addFields({ name: "標題", value: streamData.title, inline: true });
+      msgEmbed.setAuthor({ name: `${streamData.user_name}曾在 Twitch 上直播.`, iconURL: `${streamData.profile_image_url}`, url: 'https://twitch.tv/${(streamData.login || streamData.user_name).toLowerCase()}'});
     }
     
     // Add game
@@ -37,18 +38,29 @@ class LiveEmbed {
       msgEmbed.addFields({ name: "正在遊玩", value: streamData.game.name, inline: false });
     }
     
-    if (true) { //remove isLive make stats always show
-      msgEmbed.addFields({ name: "更新", value: isLive ? `:green_square: 正在追蹤` : ':black_large_square: 停止追蹤', inline: false });
-      // Add status
-      msgEmbed.addFields({ name: "狀態", value: isLive ? `直播有 ${streamData.viewer_count} 名觀眾` : '直播已結束', inline: true });
-      
+    if (isLive) {
       // Set main image (stream preview)
-      let imageUrl = streamData.thumbnail_url;
+      let imageUrl = streamData.offline_image_url;
       imageUrl = imageUrl.replace("{width}", "1280");
       imageUrl = imageUrl.replace("{height}", "720");
       let thumbnailBuster = (Date.now() / 1000).toFixed(0);
       imageUrl += `?t=${thumbnailBuster}`;
       msgEmbed.setImage(imageUrl);
+    } else {
+      let imageUrl = streamData.offline_image_url;
+      imageUrl = imageUrl.replace("{width}", "1280");
+      imageUrl = imageUrl.replace("{height}", "720");
+      let thumbnailBuster = (Date.now() / 1000).toFixed(0);
+      imageUrl += `?t=${thumbnailBuster}`;
+      msgEmbed.setImage(imageUrl);
+    }
+    
+    if (true) { //remove isLive make stats always show
+      //msgEmbed.addFields({ name: "更新", value: isLive ? `:red_circle: 正在追蹤` : ':white_circle: 停止追蹤', inline: false });
+      // Add status
+      msgEmbed.addFields({ name: "狀態", value: isLive ? `直播有 ${streamData.viewer_count} 名觀眾` : '直播已結束', inline: true });
+      
+      // Set main image (stream preview) Move to alone
       
       // Add uptime
       let now = moment();
